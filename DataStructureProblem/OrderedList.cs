f10cd6061e4d79ed6bb239d9ab11c5d9eaac061f
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-31 13:21:13 +0530

    [Kiran] Add: Ordered List

---
+++ DataStructureProblem/OrderedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    public class OrderedList
+    {
+        public void Order()
+        {
+            List<int> number = new List<int> { 1, 6, 7, 5, 9, 8 };
+            number.Sort();
+            LinkedList<int> list = new LinkedList<int>();
+            foreach (int i in number)
+            {
+                list.Add(i);
+            }
+            list.Display();
+        }
+    }
+}
